02707b2fa9906f328860f156baf9ec5db0de5101
dmitry.tsekhanovich <dmitry.tsekhanovich@solbeg.com>
2021-08-08 17:42:31 +0300

    Добавил модели, подключил bootstrap, настроил админку

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,53 @@
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+from PIL import Image
+
+
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (800, 800)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = f'Изображение не должно быть меньше '\
+                        f'{self.MIN_RESOLUTION[0]}х{self.MIN_RESOLUTION[1]}'\
+                        f' и больше '\
+                        f'{self.MAX_RESOLUTION[0]}х{self.MAX_RESOLUTION[1]}'
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Изображение слишком маленькое')
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Изображение слишком большое')
+        return image
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug=
+                                                                  'notebooks'))
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug=
+                                                                'smartphones'))
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,19 +1,39 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from  django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
 
-#############
-# 1 Category
-# 2 Product
-# 3 CartProduct
-# 4 Cart
-# 5 Order
-############
-# 6 Customer
-# 7 Specification
+class LatestProductsManager:
+
+    def get_products_for_main_page(self, *args, **kwargs):
+
+        """Метод который возвращает на стартовую страницу последние
+        добавленные товары"""
+
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.\
+                                 all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key=lambda x: x.__class__._meta.\
+                        model_name.startswith(with_respect_to), reverse=True
+                    )
+        return products
+
+
+class LatestProducts:
+
+    object = LatestProductsManager()
 
 
 class Category(models.Model):
@@ -25,6 +45,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    class Meta:
+        abstract = True
+
     category = models.ForeignKey(Category, verbose_name='Категория',
                                  on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
@@ -44,8 +67,9 @@ class CartProduct(models.Model):
     cart = models.ForeignKey('Cart', verbose_name='Корзина',
                              on_delete=models.CASCADE,
                              related_name='related_products')
-    product = models.ForeignKey(Product, verbose_name='Товар',
-                                on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     final_price = models.DecimalField(max_digits=9, decimal_places=2,
                                       verbose_name='Общая цена')
 
@@ -67,7 +91,6 @@ class Cart(models.Model):
 
 
 class Customer(models.Model):
-
     user = models.ForeignKey(User, verbose_name='Пользователь',
                              on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name='Номер телефона')
@@ -76,13 +99,42 @@ class Customer(models.Model):
     def __str__(self):
         return f"Покупатель: {self.user.first_name} {self.user.last_name}"
 
+# Ниже представлены модели товаров для магазина с перечислением
+# опциональных полей
 
-class Specification(models.Model):
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255,
-                            verbose_name='Имя товара для характеристики')
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor_freq = models.CharField(max_length=255,
+                                      verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255,
+                           verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255,
+                                           verbose_name='Время работы'
+                                                        ' аккумулятора')
 
     def __str__(self):
-        return f"Характеристики {self.name}"
+        return f"{self.category.name} : {self.title} "
+
+
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение '
+                                                               'экрана')
+    accum_volume = models.CharField(max_length=255, verbose_name='Объем '
+                                                                 'батареи')
+    ram = models.CharField(max_length=255,
+                           verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255,
+                                     verbose_name='Максимальный объем '
+                                                  'встраиваимой памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Главная'
+                                                                ' камера')
+    frontal_camera_mp = models.CharField(max_length=255,
+                                         verbose_name='Фронтальная камера')
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title} "
